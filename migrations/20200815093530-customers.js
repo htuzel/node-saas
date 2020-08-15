b03b036d8265bb345fcc728f6dcf3f8c8b8adfe2
Yusuf Borucu <yusufborucu@gmail.com>
2020-08-15 14:33:58 +0300

    migrations edit

--- migrations/20200815093530-customers.js
+++ migrations/20200815093530-customers.js
@@ -5,26 +5,45 @@ module.exports = {
       {
         "name": "test",
         "email": "test@email.com",
-        "password": "123456"
+        "password": "123456",
+        "db_infos": {
+          "db_name": "customer1_db",
+          "db_host": "localhost",
+          "db_username": "customer1_user",
+          "db_password": "123456",
+          "db_port": "27017"
+        },
+        "api_keys": {
+          "client_id": "",
+          "client_secret": ""
+        },
+        "created_at": new Date(),
+        "updated_at": new Date()
       }
     );
     await db.collection('customers').insertOne(
       {
         "name": "test2",
         "email": "test2@email.com",
-        "password": "123456"
+        "password": "123456",
+        "db_infos": {
+          "db_name": "customer2_db",
+          "db_host": "localhost",
+          "db_username": "customer2_user",
+          "db_password": "123456",
+          "db_port": "27017"
+        },
+        "api_keys": {
+          "client_id": "",
+          "client_secret": ""
+        },
+        "created_at": new Date(),
+        "updated_at": new Date()
       }
     );
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
   },
 
   async down(db, client) {
     return db.collection('customers').drop();
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
   }
 };
